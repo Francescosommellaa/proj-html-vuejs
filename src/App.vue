4bbee9a55c5ae208e934c37cd9117e3ec524b8e3
Francescosommellaa <franc.sommella@gmail.com>
2024-07-16 17:44:30 +0200

    header condivisione problema con router-link

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,21 @@
 <script>
 import AppFooter from './components/AppFooter.vue';
 import AppHeader from './components/AppHeader.vue';
+import Home from './Pages/Home.vue';
+import NotFound from './Pages/NotFound.vue';
+import OurTeam from './Pages/OurTeam.vue';
+import WhatWeOffer from './Pages/WhatWeOffer.vue';
 import { store } from './store';
 
 export default {
   name: 'App',
   components: {
     AppHeader,
-    AppFooter
+    AppFooter,
+    Home,
+    OurTeam,
+    WhatWeOffer,
+    NotFound,
   },
   data() {
     return {
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,12 +1,25 @@
 <script>
-import { store } from '../store'
+import Home from '../Pages/Home.vue';
+import NotFound from '../Pages/NotFound.vue';
+import OurTeam from '../Pages/OurTeam.vue';
+import WhatWeOffer from '../Pages/WhatWeOffer.vue';
+import { store } from '../store';
+
+
 export default {
   name: 'AppHeader',
+  components: {
+    Home,
+    OurTeam,
+    WhatWeOffer,
+    NotFound,
+  },
   data() {
     return {
       store,
     }
   },
+  
 }
 
 
@@ -15,9 +28,22 @@ export default {
 
 <template>
 
-  <div>
-    
-  </div>
+  <nav class="navbar navbar-expand">
+    <img src="../../public/logo-sidearea-1-1.png" alt="Logo Marèe">
+    <div>
+      <ul>
+          <li>
+              <router-link :to="{ name: Home }">Home</router-link>
+          </li>
+          <li>
+              <router-link :to="{ name: OurTeam }">OurTeam</router-link>
+          </li>
+          <li>
+              <router-link :to="{ name: WhatWeOffer }">WhatWeOffer</router-link>
+          </li>
+        </ul>
+    </div>
+  </nav>
 
 </template>
 
