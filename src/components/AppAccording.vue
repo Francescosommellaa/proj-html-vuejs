7a4dc59eb9089948cca80bde676831d188f4a0b1
Nicolò Pincelli <pincelli.n@gmail.com>
2024-07-17 17:16:12 +0200

    Firstrealising App According

--- src/components/AppAccording.vue
+++ src/components/AppAccording.vue
@@ -1,25 +1,7 @@
 <script>
 export default {
     name: "AppAccording",
-    computed: {
-        accord() {
-            var acc = document.getElementsByClassName("accordion");
-            var i;
 
-            for (i = 0; i < acc.length; i++) {
-                acc[i].addEventListener("click", function () {
-                    this.classList.toggle("active");
-                    var panel = this.nextElementSibling;
-                    if (panel.style.maxHeight) {
-                        panel.style.maxHeight = null;
-                    } else {
-                        panel.style.maxHeight = panel.scrollHeight + "px";
-                    }
-                });
-            }
-        }
-
-    }
 }
 
 
@@ -27,70 +9,78 @@ export default {
 
 <template>
 
-    <button @click="this.accord()" class="accordion">Animation</button>
-    <div class="panel">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
-            ea commodo consequat.</p>
+    <div class="accordion" id="accordionExample">
+        <div class="accordion-item">
+            <h2 class="accordion-header">
+                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne"
+                    aria-expanded="false" aria-controls="collapseOne">
+                    Animation
+                </button>
+            </h2>
+            <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
+                    et
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip ex
+                    ea commodo consequat.
+                </div>
+            </div>
+        </div>
+        <div class="accordion-item">
+            <h2 class="accordion-header">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                    Riggering
+                </button>
+            </h2>
+            <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
+                    et
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip ex
+                    ea commodo consequat.
+                </div>
+            </div>
+        </div>
+        <div class="accordion-item">
+            <h2 class="accordion-header">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                    Special Effects
+                </button>
+            </h2>
+            <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
+                    et
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip ex
+                    ea commodo consequat.
+                </div>
+            </div>
+        </div>
+        <div class="accordion-item">
+            <h2 class="accordion-header">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+                    3D Modeling
+                </button>
+            </h2>
+            <div id="collapseFour" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore
+                    et
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip ex
+                    ea commodo consequat.
+                </div>
+            </div>
+        </div>
     </div>
 
-    <button class="accordion">Riggering</button>
-    <div class="panel">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
-            ea commodo consequat.</p>
-    </div>
 
-    <button class="accordion">Special Effects</button>
-    <div class="panel">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
-            ea commodo consequat.</p>
-    </div>
-    <button class="accordion">3D Modeling</button>
-    <div class="panel">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
-            ea commodo consequat.</p>
-    </div>
 </template>
 
-<style lang="scss" scoped>
-.accordion {
-    background-color: #eee;
-    color: #444;
-    cursor: pointer;
-    padding: 18px;
-    width: 100%;
-    border: none;
-    text-align: left;
-    outline: none;
-    font-size: 15px;
-    transition: 0.4s;
-}
-
-.active,
-.accordion:hover {
-    background-color: #ccc;
-}
-
-.accordion:after {
-    content: '\002B';
-    color: #777;
-    font-weight: bold;
-    float: right;
-    margin-left: 5px;
-}
-
-.active:after {
-    content: "\2212";
-}
-
-.panel {
-    padding: 0 18px;
-    background-color: white;
-    max-height: 0;
-    overflow: hidden;
-    transition: max-height 0.2s ease-out;
-}
-</style>
+<style lang="scss" scoped></style>
--- src/components/AppTabs.vue
+++ src/components/AppTabs.vue
@@ -7,14 +7,14 @@ export default {
         }
     },
     methods: {
-        openCity(cityName, buttonName) {
+        opendescription(sector, buttonName) {
             let bordercol = document.getElementsByClassName("col-4")
-            var x = document.getElementsByClassName("city");
+            var x = document.getElementsByClassName("description");
             for (let i = 0; i < x.length; i++) {
                 x[i].style.display = "none";
                 bordercol[i].classList.remove("activ-color");
             }
-            document.getElementById(cityName).style.display = "block";
+            document.getElementById(sector).style.display = "block";
             document.getElementById(buttonName).classList.add("activ-color");
             console.log(document.getElementById(buttonName));
         }
@@ -28,20 +28,20 @@ export default {
             <div class="row border-bottom   d-flex justify-content-between">
                 <div id="buttoAnimation" class="col-4 text-start activ-color"> <button
                         class="border-0 bg-transparent text-uppercase "
-                        @click="this.openCity('Animation', 'buttoAnimation')">Animation</button>
+                        @click="this.opendescription('Animation', 'buttoAnimation')">Animation</button>
                 </div>
                 <div id="buttonDesign" class="col-4 text-center"> <button
                         class="border-0  bg-transparent text-uppercase "
-                        @click="this.openCity('Design', 'buttonDesign')">Design</button></div>
+                        @click="this.opendescription('Design', 'buttonDesign')">Design</button></div>
                 <div id="buttonLettering" class="col-4 text-end"> <button
                         class="border-0 bg-transparent text-uppercase text-end"
-                        @click="this.openCity('Lettering', 'buttonLettering')">Lettering</button>
+                        @click="this.opendescription('Lettering', 'buttonLettering')">Lettering</button>
                 </div>
             </div>
 
         </div>
         <div class="col-10">
-            <div id="Animation" class="w3-container city">
+            <div id="Animation" class="w3-container description">
 
                 <p class='text-left'>Hinc assum pro ea. Meis verear an vel, ex quando legere scriptorem has, mea vide
                     vidisse
@@ -51,7 +51,7 @@ export default {
                     vix propriae deseruisse. Munere utamur vertere.</p>
             </div>
 
-            <div id="Design" class="w3-container city" style="display:none">
+            <div id="Design" class="w3-container description" style="display:none">
 
                 <p class='text-left'>Paris is the capital of France.
                     Integre appareat cu mea, te mel ferri percipitur efficiendi, in ius phaedrum invenire expetendis.
@@ -62,7 +62,7 @@ export default {
                     propriae
                     deseruisse. Inani expetendis ut cum.</p>
             </div>
-            <div id="Lettering" class="w3-container city" style="display:none">
+            <div id="Lettering" class="w3-container description" style="display:none">
 
                 <p class='text-left'>Te sale iusto fabellas sed, eu sea tollit suscipiantur. Oblique omnesque ne eum, id
                     putent malorum
