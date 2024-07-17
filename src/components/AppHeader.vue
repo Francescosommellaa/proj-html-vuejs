a19d88cfef572a7ec9fcca80da9e0bef1f942f10
Francescosommellaa <franc.sommella@gmail.com>
2024-07-17 11:15:24 +0200

    completato basic header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -9,26 +9,29 @@ export default {
 
 <template>
 
-  <nav class="navbar navbar-expand">
+  <nav class="navbar">
     <img src="../../public/logo-sidearea-1-1.png" alt="Logo Marèe">
-    <div>
-      <ul>
-        <li>
-          <router-link :to="{ name: 'Home' }" class="nav-link">
-            Home
-          </router-link>
-        </li>
-        <li>
-          <router-link :to="{ name: 'OurTeam' }" class="nav-link">
-            OurTeam
-          </router-link>
-        </li>
-        <li>
-          <router-link :to="{ name: 'WhatWeOffer' }" class="nav-link">
-            WhatWeOffer
-          </router-link>
-        </li>
-      </ul>
+    <div class="d-flex align-items-center ">
+      <div>
+        <ul class="d-flex align-items-center">
+          <li class="ms-listlink">
+            <router-link :to="{ name: 'Home' }" class="nav-link">
+              Home
+            </router-link>
+          </li>
+          <li class="ms-listlink">
+            <router-link :to="{ name: 'OurTeam' }" class="nav-link">
+              OurTeam
+            </router-link>
+          </li>
+          <li class="ms-listlink">
+            <router-link :to="{ name: 'WhatWeOffer' }" class="nav-link">
+              WhatWeOffer
+            </router-link>
+          </li>
+        </ul>
+      </div>
+      <img class="menu-header" src="../../public/svg/svg-1.svg" alt="Logo Marèe">
     </div>
   </nav>
 
@@ -36,4 +39,17 @@ export default {
 
 <style lang="scss" scoped>
 @use '../styles/general.scss';
+@use '../styles/variabiles' as *;
+
+.d-flex {
+  gap: 2em;
+  height: 60px;
+  margin: 0;
+  font-size: 18px;
+  font-weight: 700;
+
+  .menu-header {
+    height: 24px;
+  }
+}
 </style>
