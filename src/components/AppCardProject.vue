86237aa6315bfe4859b460db5b034fc92fb3fdf1
Francescosommellaa <franc.sommella@gmail.com>
2024-07-17 16:01:36 +0200

    creato appcardproject

---
+++ src/components/AppCardProject.vue
@@ -0,0 +1,39 @@
+<script>
+import { store } from '../store'
+
+export default {
+    name: 'AppCardProject',
+    data() {
+        return {
+            store,
+        }
+    },
+}
+</script>
+
+<template>
+<div class="container-card d-flex flex-wrap">
+    <div v-for="(item, index) in store.illustrations" :key="index" class="card">
+        <img :src="item.pic" alt="Illustration" />
+        <div class="text">
+            {{ item.title }}
+        </div>
+    </div>
+</div>
+</template>
+
+<style lang="scss" scoped>
+@use '../styles/general.scss';
+@use '../styles/variabiles' as *;
+
+.card {
+    width: calc(100% / 3);
+
+    .text {
+        height: 40px;
+        width: 80px;
+        background-color: #fff;
+    }
+}
+
+</style>
