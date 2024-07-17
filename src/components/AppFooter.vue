576889e23c39e80d1f5860762c155a6cf00746bc
Francescosommellaa <franc.sommella@gmail.com>
2024-07-17 12:15:30 +0200

    fix footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -17,7 +17,8 @@ export default {
 
   <div class="row ms-bg">
     <div class="col3">
-      <img src="../../public/logo-sidearea-1-1.png" alt="">
+      <img src="../../public/logo-sidearea-1-1.png" alt="logo footer">
+      <span>Let's Get Creative</span>
     </div>
     <div class="col3"></div>
     <div class="col3"></div>
@@ -28,7 +29,7 @@ export default {
 
 <style lang="scss" scoped>
 @use '../styles/general.scss';
-@use '../styles/variabiles';
+@use '../styles/variabiles' as *;
 
 .ms-bg {
   background-color: $backgroud-color8;
