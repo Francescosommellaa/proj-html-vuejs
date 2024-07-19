9e80afab13c21cbdf554c5dfb5a412fde1b5d572
Nicolò Pincelli <pincelli.n@gmail.com>
2024-07-19 09:21:44 +0200

    Complete App Tabs

--- src/components/AppProgres.vue
+++ src/components/AppProgres.vue
@@ -28,12 +28,12 @@ export default {
         };
     },
     methods: {
-        incrementCounter(index) {
+        increment(index) {
             const targetPercent = this.areaWorks[index].percent;
-            this.areaWorks[index].currentPercent = 0;
+            this.areaWorks[index].incris = 0;
             const interval = setInterval(() => {
-                if (this.areaWorks[index].currentPercent < targetPercent) {
-                    this.areaWorks[index].currentPercent++;
+                if (this.areaWorks[index].incris < targetPercent) {
+                    this.areaWorks[index].incris++;
                 } else {
                     clearInterval(interval);
                 }
@@ -42,22 +42,22 @@ export default {
     },
     mounted() {
         this.areaWorks.forEach((work, index) => {
-            this.incrementCounter(index);
+            this.increment(index);
         });
     },
 };
 </script>
 
 <template>
-    <div v-for="work in this.areaWorks" :key="index" class="row">
+    <div v-for="(work, index) in this.areaWorks" :key="index" class="row">
         <div class="col-12 mx-auto d-flex justify-content-between align-items-center">
             <h5>{{ work.area }}</h5>
-            <h6>{{ work.currentPercent }}%</h6>
+            <h6>{{ work.incris }}%</h6>
         </div>
         <div class="col-12 mx-auto">
-            <div class="progress" role="progressbar" aria-label="4px high" aria-valuenow="{{work.percent}} "
+            <div class="progress" role="progressbar" aria-label="4px high" :aria-valuenow="work.percent"
                 aria-valuemin="0" aria-valuemax="100" style="height: 4px">
-                <div class="progress-bar" :style="{ width: work.currentPercent + '%' }"></div>
+                <div class="progress-bar" :style="{ width: work.incris + '%' }"></div>
             </div>
         </div>
     </div>
--- src/components/AppTabs.vue
+++ src/components/AppTabs.vue
@@ -2,21 +2,27 @@
 export default {
     name: "AppTabs",
     data() {
-        return {};
-    },
-    methods: {
-        opendescription(sector, buttonName) {
-            let bordercol = document.getElementsByClassName("col-4");
-            var x = document.getElementsByClassName("description");
-            for (let i = 0; i < x.length; i++) {
-                x[i].style.display = "none";
-                bordercol[i].classList.remove("activ-color");
+        return {
+            activestate: 'idAnimation',
+            tabsArea: [{
+                id: 'idAnimation',
+                title: 'Animation',
+                text: 'Hinc assum pro ea. Meis verear an vel, ex quando legere scriptorem has, mea vide vidisse an.Eius eirmod vel in, quo simul salutatus incorrupte ex, nisl nonumes democritum per te.Ex mei illud illum verter.'
+            },
+            {
+                id: 'idDesing',
+                title: 'Desing',
+                text: ' Paris is the capital of France. Integre appareat cu mea, te mel ferripercipitur efficiendi, in ius phaedrum invenire expetendis.Eius eirmod vel in, quo simul salutatus incorrupte ex, nisl nonumesdemocritum per te.Ex mei illud illum laudem, quem bonorum ius ne.His an omnes propriae fabellas, an duo aeterno expetenda explicari, ea vix propriae deseruisse.Inani expetendis ut cum.'
+            },
+            {
+                id: 'idLettering',
+                title: 'Lettering',
+                text: ' Te sale iusto fabellas sed, eu sea tollit suscipiantur. Obliqueomnesque ne eum, id putent malorum admodum vel, vix no tantas admodum nominavi.Eius eirmod vel in, quo simul salutatus incorrupte ex, nislnonumes democritum per te.Ex mei illud illum laudem, quem bonorum iusne.His an omnes propriae fabellas, an duo aeterno expetend.'
             }
-            document.getElementById(sector).style.display = "block";
-            document.getElementById(buttonName).classList.add("activ-color");
-            console.log(document.getElementById(buttonName));
-        },
+            ]
+        };
     },
+
 };
 </script>
 
@@ -24,55 +30,20 @@ export default {
     <div class="row">
         <div class="col-10">
             <div class="row border-bottom d-flex justify-content-between">
-                <div id="buttoAnimation" class="col-4 text-start activ-color">
-                    <button class="border-0 bg-transparent text-uppercase"
-                        @click="this.opendescription('Animation', 'buttoAnimation')">
-                        Animation
+                <div v-for="tab in tabsArea" :id="tab.id" class="col-4 text-start"
+                    :class="tab.id == activestate ? 'activ-color' : ''">
+                    <button class="border-0 bg-transparent text-uppercase" @click="activestate = tab.id">
+                        {{ tab.title }}
                     </button>
                 </div>
-                <div id="buttonDesign" class="col-4 text-center">
-                    <button class="border-0 bg-transparent text-uppercase"
-                        @click="this.opendescription('Design', 'buttonDesign')">
-                        Design
-                    </button>
-                </div>
-                <div id="buttonLettering" class="col-4 text-end">
-                    <button class="border-0 bg-transparent text-uppercase text-end"
-                        @click="this.opendescription('Lettering', 'buttonLettering')">
-                        Lettering
-                    </button>
-                </div>
+
             </div>
         </div>
         <div class="col-10">
-            <div id="Animation" class="w3-container description">
+            <div v-for="tab in tabsArea" class="w3-container description"
+                :class='tab.id == activestate ? "d-block" : "d-none"'>
                 <p class="text-left">
-                    Hinc assum pro ea. Meis verear an vel, ex quando legere scriptorem
-                    has, mea vide vidisse an. Eius eirmod vel in, quo simul salutatus
-                    incorrupte ex, nisl nonumes democritum per te. Ex mei illud illum
-                    laudem, quem bonorum ius ne. His an omnes propriae fabellas, an duo
-                    aeterno expetenda explicari, ea vix propriae deseruisse. Munere utamur
-                    vertere.
-                </p>
-            </div>
-
-            <div id="Design" class="w3-container description" style="display: none">
-                <p class="text-left">
-                    Paris is the capital of France. Integre appareat cu mea, te mel ferri
-                    percipitur efficiendi, in ius phaedrum invenire expetendis. Eius
-                    eirmod vel in, quo simul salutatus incorrupte ex, nisl nonumes
-                    democritum per te. Ex mei illud illum laudem, quem bonorum ius ne. His
-                    an omnes propriae fabellas, an duo aeterno expetenda explicari, ea vix
-                    propriae deseruisse. Inani expetendis ut cum.
-                </p>
-            </div>
-            <div id="Lettering" class="w3-container description" style="display: none">
-                <p class="text-left">
-                    Te sale iusto fabellas sed, eu sea tollit suscipiantur. Oblique
-                    omnesque ne eum, id putent malorum admodum vel, vix no tantas admodum
-                    nominavi. Eius eirmod vel in, quo simul salutatus incorrupte ex, nisl
-                    nonumes democritum per te. Ex mei illud illum laudem, quem bonorum ius
-                    ne. His an omnes propriae fabellas, an duo aeterno expetend.
+                    {{ tab.text }}
                 </p>
             </div>
         </div>
